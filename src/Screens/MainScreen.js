38b95d5847d011b12dcc9764ee1fcbccb807604e
NizamJajam <58840684+NizamJajam@users.noreply.github.com>
2021-12-29 20:57:52 +0800

    More updates 29/12/2021

--- src/Screens/MainScreen.js
+++ src/Screens/MainScreen.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { View, Text, StyleSheet, Dimensions, Image, TouchableOpacity } from 'react-native';
-import { FaSearch, FaRegPlayCircle, FaGooglePlay, FaApple, } from "react-icons/fa"
-// const windowHeight = Dimensions.get('window').height;
+import {
+    FaSearch, FaRegPlayCircle, FaGooglePlay, FaApple, FaFacebookSquare, FaInstagramSquare, FaTwitterSquare,
+} from "react-icons/fa"
 const { width, height } = Dimensions.get('window');
 
 export default class MainScreen extends React.Component {
@@ -179,7 +180,101 @@ export default class MainScreen extends React.Component {
                     </View>
                 </View>
                 <View style={styles.aboutSection}>
+                    <View style={styles.harpyAddress}>
+                        <View style={{ width: '90%', height: '100%' }}>
+                            <Text style={{ color: '#666666', fontSize: 30, fontWeight: 'bold'}}>HARPY</Text>
+                            <Text style={{ color: '#666666', fontSize: 12, }}>Because Time is Precious</Text>
+                            <Text style={{ color: '#666666', fontSize: 15, }}>
+                                3rd Floor, Lot 29, Block D,
+                                J/n Singgah Mata 1, Asia City,
+                                88000 Kota Kinabalu,
+                                Sabah
+                            </Text>
+                        </View>
 
+                    </View>
+                    <View style={styles.aboutCategories}>
+                        <View style={styles.categoryHeader}>
+                            <Text style={{ color: '#666666', fontSize: 15, fontWeight:"bold", }}>About</Text>
+                        </View>
+                        <View style={styles.categoryBody}>
+                            <Text style={{ color: '#666666', fontSize: 13, }}>Brand Story </Text>
+                            <Text style={{ color: '#666666', fontSize: 13, }}>Locations </Text>
+                            <Text style={{ color: '#666666', fontSize: 13, }}>Trust & Safety </Text>
+                            <Text style={{ color: '#666666', fontSize: 13, }}>Careers </Text>
+                            <Text style={{ color: '#666666', fontSize: 13, }}> </Text>
+                        </View>
+                    </View>
+                    <View style={styles.aboutCategories}>
+                        <View style={styles.categoryHeader}>
+                            <Text style={{ color: '#666666', fontSize: 15, fontWeight:"bold", }}>Consumer</Text>
+                        </View>
+                        <View style={styles.categoryBody}>
+                            <Text style={{ color: '#666666', fontSize: 13, }}>What's New </Text>
+                            <Text style={{ color: '#666666', fontSize: 13, }}>HarpyMail </Text>
+                            <Text style={{ color: '#666666', fontSize: 13, }}>HarpyFood </Text>
+                            <Text style={{ color: '#666666', fontSize: 13, }}>HarpyMart </Text>
+                            <Text style={{ color: '#666666', fontSize: 13, }}>HarpyPay </Text>
+                        </View>
+                    </View>
+                    <View style={styles.aboutCategories}>
+                        <View style={styles.categoryHeader}>
+                            <Text style={{ color: '#666666', fontSize: 15, fontWeight:"bold", }}>Harpy Rider</Text>
+                        </View>
+                        <View style={styles.categoryBody}>
+                            <Text style={{ color: '#666666', fontSize: 13, }}>What's New </Text>
+                            <Text style={{ color: '#666666', fontSize: 13, }}>Transport </Text>
+                            <Text style={{ color: '#666666', fontSize: 13, }}>HarpyMail Partner </Text>
+                            <Text style={{ color: '#666666', fontSize: 13, }}>Rider Centre </Text>
+                            <Text style={{ color: '#666666', fontSize: 13, }}> </Text>
+                        </View>
+                    </View>
+                    <View style={styles.aboutCategories}>
+                        <View style={styles.categoryHeader}>
+                            <Text style={{ color: '#666666', fontSize: 15, textAlign: 'center', fontWeight:"bold", }}>Harpy Cloud Runner</Text>
+                        </View>
+                        <View style={styles.categoryBody}>
+                            <Text style={{ color: '#666666', fontSize: 13, }}>What's New </Text>
+                            <Text style={{ color: '#666666', fontSize: 13, }}>Transport </Text>
+                            <Text style={{ color: '#666666', fontSize: 13, }}>HarpyMail Partner </Text>
+                            <Text style={{ color: '#666666', fontSize: 13, }}>Rider Centre </Text>
+                            <Text style={{ color: '#666666', fontSize: 13, }}> </Text>
+                        </View>
+                    </View>
+                    <View style={styles.aboutCategories}>
+                        <View style={styles.categoryHeader}>
+                            <Text style={{ color: '#666666', fontSize: 15, fontWeight:"bold", }}>Quick Links</Text>
+                        </View>
+                        <View style={styles.categoryBody}>
+                            <Text style={{ color: '#666666', fontSize: 13, }}>What's New </Text>
+                            <Text style={{ color: '#666666', fontSize: 13, }}>Food Menu </Text>
+                            <Text style={{ color: '#666666', fontSize: 13, }}>COVID-19 Updates </Text>
+                            <Text style={{ color: '#666666', fontSize: 13, }}>Developer Tools </Text>
+                            <Text style={{ color: '#666666', fontSize: 13, }}> </Text>
+                        </View>
+                    </View>
+                    <View style={styles.harpyAddress}>
+                        <View style={{ width: '90%', height: '100%' }}>
+                            <View style={styles.socMedTop}>
+                                <Text style={{ color: '#666666', fontSize: 15, }}>Follow Us and Keep Updated</Text>
+                            </View>
+                            <View style={styles.socMedMid}>
+                                <FaInstagramSquare color='#FFAC18' size={40} />
+                                <FaFacebookSquare color='#FFAC18' size={40} />
+                                <FaTwitterSquare color='#FFAC18' size={40} />
+                            </View>
+                            <View style={styles.socMedBottom}>
+                                <TouchableOpacity style={styles.buttons4}>
+                                    <FaGooglePlay color="white" />
+                                    <Text style={styles.buttonText}>Google Play</Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity style={styles.buttons4}>
+                                    <FaApple color="white" />
+                                    <Text style={styles.buttonText}>App Store</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </View>
                 </View>
                 <View style={styles.footer}>
                     <View style={{ flexDirection: 'row' }}>
@@ -375,6 +470,28 @@ const styles = StyleSheet.create({
         width: '100%',
         justifyContent: 'center',
         alignItems: 'center',
+        flexDirection: 'row',
+    },
+    harpyAddress: {
+        paddingHorizontal: 10,
+        flex: 2,
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    aboutCategories: {
+        flex: 1,
+        height: '100%',
+    },
+    categoryHeader: {
+        flex: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    categoryBody: {
+        flex: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
     footer: {
         height: 150,
@@ -386,5 +503,32 @@ const styles = StyleSheet.create({
     footerText: {
         color: 'white',
         fontSize: 15,
+    },
+    socMedTop: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },
+    socMedMid: {
+        flex: 1,
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        flexDirection: 'row',
+    },
+    socMedBottom: {
+        flex: 1,
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        flexDirection: 'row'
+    },
+    buttons4: {
+        backgroundColor: '#FFAC18',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        flexDirection: 'row',
+        borderRadius: 5,
+        width: '40%',
+        height: '90%',
     },
 })
